e8e60743b87a28269211ba4ea64618f339c8dfe2
ramvirsingh <ramvir.rkgit@gmail.com>
2016-12-24 02:27:10 +0530

    crud api

--- src/main/java/com/springbootdemo/web/UserController.java
+++ src/main/java/com/springbootdemo/web/UserController.java
@@ -4,12 +4,16 @@
 package com.springbootdemo.web;
 
 
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.springbootdemo.business.UserService;
 import com.springbootdemo.model.User;
 
 /**
@@ -19,14 +23,29 @@ import com.springbootdemo.model.User;
 @RestController
 public class UserController {
 
-	@RequestMapping("/user")
+	@Autowired
+	private UserService userService;
+
+	@RequestMapping("/users")
 	public List <User> listUsers(){
-		return Arrays.asList(
-				new User("RAM",20,"Noida"),
-				new User("RAM",20,"Noida"),
-				new User("RAM",20,"Noida"),
-				new User("RAM",20,"Noida")
-				);
+		return userService.listUsers();
 	}
 
+	@RequestMapping("/users/{username}")
+	public User getUser(@PathVariable String username){
+		return userService.getUser(username);
+	}
+
+	@RequestMapping(method=RequestMethod.POST, value="/users")
+	public void addUser(@RequestBody User user){
+		userService.addUser(user);
+	}
+	@RequestMapping(method=RequestMethod.PUT, value="/users/{username}")
+	public void addUser(@RequestBody User user,@PathVariable String username){
+		userService.updateUser(username,user);
+	}
+	@RequestMapping(method=RequestMethod.DELETE,value="/users/{username}")
+	public void deleteUser(@PathVariable String username){
+		 userService.deleteUser(username);
+	}
 }
